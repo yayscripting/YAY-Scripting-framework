2382430cdafcc502617b72dee13b3374518e3cbe
Djekkoo <djekkoo@hotmail.com>
2012-01-10 16:50:13 +0100

    Renamed debug-title, because the layout-class takes this name.

--- application/config/script.cfg.php
+++ application/config/script.cfg.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 
-	'debug' => true,		// enable smarty debugging
+	'debug_mode' => true,		// enable smarty debugging
 	'show_render_time' => true,	// Show render time in HTML-source code.
 	'default_controller' => 'home'
 	
