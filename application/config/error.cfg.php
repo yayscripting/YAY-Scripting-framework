3414a3963c9bf2b8944b7645c4424594090b1f59
Djekkoo <djekkoo@hotmail.com>
2012-01-16 16:44:27 +0100

    Actually adding the E_WARNING i warned for.

--- application/config/error.cfg.php
+++ application/config/error.cfg.php
@@ -2,7 +2,7 @@
 return array(
 
 	'register_error_handler'     => true,
-	'error_handler_types' 	     => E_ALL ^ ( E_DEPRECATED  | E_USER_DEPRECATED  | E_NOTICE  ), //E_WARNING causes even errors when MySQL-server is offline, so make sure that is not selected.
+	'error_handler_types' 	     => E_ALL ^ ( E_DEPRECATED  | E_USER_DEPRECATED  | E_NOTICE | E_WARNING  ), //E_WARNING causes even errors when MySQL-server is offline, so make sure that is not selected.
 	'register_exception_handler' => true,
 	'fatal_error_handler' 	     => true
 	
